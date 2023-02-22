52a41862ae9f33d1a407d208383807da905dbb86
nikhil-223 <113900755+nikhil-223@users.noreply.github.com>
2023-02-23 02:37:27 +0530

    exchange coins component edited

--- src/components/dashboard/exchangeCoins/ExchangeCoins.js
+++ src/components/dashboard/exchangeCoins/ExchangeCoins.js
@@ -1,10 +1,41 @@
-import React from 'react'
-import './ExchangeCoins.scss'
+import React from "react";
+import "./ExchangeCoins.scss";
+import { AiOutlineCaretDown } from "react-icons/ai";
 
 const ExchangeCoins = () => {
-  return (
-    <div className='exchange-coins'>ExchangeCoins</div>
-  )
-}
+	return (
+		<div className="exchange-coins">
+			<div className="exchange-coins__title">Exchange Coins</div>
+			<div className="exchange-coins__sell">
+				<span>Sell</span>
+				<div className="exchange-coins__sell__drop">
+					<span>Bitcoin</span>
+					<div className="exchange-coins__sell__drop__icon">
+						<AiOutlineCaretDown />
+					</div>
+				</div>
+			</div>
+			<div className="exchange-coins__buy">
+				<span>Buy</span>
+				<div className="exchange-coins__buy__drop">
+					<span>Ethereum</span>
+					<div className="exchange-coins__buy__drop__icon">
+						<AiOutlineCaretDown />
+					</div>
+				</div>
+			</div>
+			<div className="exchange-coins__enter-sellValue">
+				<label htmlFor="sell">Enter value</label>
+				<input type="text" htmlFor="sell" placeholder="Avl:0.0002BTC" />
+			</div>
+			<div className="exchange-coins__get-buyValue">
+				<span> 23000Eth</span>
+			</div>
+			<div className="exchange-coins__exchange">
+				<div className="exchange-coins__exchangeBtn">Exchange</div>
+			</div>
+		</div>
+	);
+};
 
-export default ExchangeCoins
+export default ExchangeCoins;
